650c52213f8a524ab36848b88ec67a8059ee203b
npcScript <felipe.wd.dias@outlook.com>
2019-06-12 18:45:32 -0300

    Update challenge-05.js

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -2,17 +2,19 @@
 Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
-// ?
+var edifier = [ "HeadPhone", 800, true, NaN, null ]
 
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
 */
-// ?
+function ed(x) {
+  return x;
+}
 
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
 */
-// ?
+consoloe.log( ed( edifier )[1] ); // 800
 
 /*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
@@ -20,18 +22,24 @@ segundo, um número. A função deve retornar o valor de um índice do array que
 no primeiro parâmetro. O índice usado para retornar o valor, deve ser o número passado no
 segundo parâmetro.
 */
-// ???
+function twop( x, y ) {
+  return x[y];
+}
 
 /*
 Declare uma variável que recebe um array com 5 valores, de tipos diferentes.
 */
-// ?
+var read = [ "SerialKillers", 7, true, "DarkSide", 10 ]
 
 /*
 Invoque a função criada acima, fazendo-a retornar todos os valores do último
 array criado.
 */
-// ?
+console.log( twop( read, 0) );
+console.log( twop( read, 1) );
+console.log( twop( read, 2) );
+console.log( twop( read, 3) );
+console.log( twop( read, 4) );
 
 /*
 Crie uma função chamada `book`, que recebe um parâmetro, que será o nome do
@@ -47,19 +55,27 @@ propriedades:
 - Se o parâmetro não for passado, a função deve retornar o objeto com todos
 os livros.
 */
-// ?
+function book(x) {
+  var desc = {
+    "Mitologia nórdica": { quantidadePaginas: 300, autor: "Neil Gaiman", editora: "Intrínseca" },
+    "Em algum lugar nas estrelas": { quantidadePaginas: 350, autor: "Clare Vanderpool", editora: "DarkSide" },
+    "A guerra que salvou minha vida": { quantidadePaginas: 400, autor: "Kimberly Brubaker Bradley", editora: "Intrínseca" }
+  };
+
+  return !x ? desc : desc[ x ];
+}
 
 /*
 Usando a função criada acima, imprima o objeto com todos os livros.
 */
-// ?
+book();
 
 /*
 Ainda com a função acima, imprima a quantidade de páginas de um livro qualquer,
 usando a frase:
 "O livro [NOME_DO_LIVRO] tem [X] páginas!"
 */
-// ?
+console.log( "O livro Mitologia nórdica tem " book("Mitologia nórdica").quantidadePaginas " páginas!" )
 
 /*
 Ainda com a função acima, imprima o nome do autor de um livro qualquer, usando
